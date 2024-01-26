2a8eed98d72693356a8689b5aaca51cf09b2d825
Renhuhu <149178550+Renhuhu@users.noreply.github.com>
2024-01-26 10:00:35 +0800

    Add files via upload

---
+++ ESN_t2.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import scipy.sparse as sparse
+from scipy.sparse import linalg
+# import pandas as pd
+import time
+import matplotlib.pyplot as plt
+
+shift_k = 0
+
+approx_res_size = 1000
+
+
+model_params = {'tau': 0.025,
+                'nstep': 1000,
+                'N': 3,
+                'd': 3}
+
+res_params = {'radius':0.4,
+             'degree': 100,
+             'sigma': 1.0,
+             'train_length': 10000,
+             'N': int(np.floor(approx_res_size/model_params['N']) * model_params['N']),
+             'num_inputs': model_params['N'],
+             'predict_length': 5000,
+             'beta': 0.00001
+              }
+
+def generate_reservoir(size,radius,degree):
+    sparsity = degree/float(size);
+    A = sparse.rand(size,size,density=sparsity).todense()
+    vals = np.linalg.eigvals(A)
+    e = np.max(np.abs(vals))
+    A = (A/e) * radius
+    return A
+
+def reservoir_layer(A, Win, input, res_params):
+    states = np.zeros((res_params['N'],res_params['train_length']))
+    for i in range(res_params['train_length']-1):
+        states[:,i+1] = np.tanh(np.dot(A,states[:,i]) + np.dot(Win,input[:,i]))
+    return states
+
+def train_reservoir(res_params, data):
+    A = generate_reservoir(res_params['N'], res_params['radius'], res_params['degree'])
+    q = int(res_params['N']/res_params['num_inputs'])
+    Win = np.zeros((res_params['N'],res_params['num_inputs']))
+    for i in range(res_params['num_inputs']):
+        np.random.seed(seed=i)
+        Win[i*q: (i+1)*q,i] = res_params['sigma'] * (-1 + 2 * np.random.rand(1,q)[0])
+    states = reservoir_layer(A, Win, data, res_params)
+    Wout = train(res_params, states, data)
+    x = states[:,-1]
+    return x, Wout, A, Win
+
+def train(res_params,states,data):
+    beta = res_params['beta']
+    idenmat = beta * sparse.identity(res_params['N'])
+    states2 = states.copy()
+    for j in range(2,np.shape(states2)[0]-2):
+        if (np.mod(j,2)==0):
+            states2[j,:] = (states[j-1,:]*states[j-2,:]).copy()
+    U = np.dot(states2,states2.transpose()) + idenmat
+    Uinv = np.linalg.inv(U)
+    Wout = np.dot(Uinv,np.dot(states2,data.transpose()))
+    return Wout.transpose()
+
+def predict(A, Win, res_params, x, Wout):
+    output = np.zeros((res_params['num_inputs'],res_params['predict_length']))
+    for i in range(res_params['predict_length']):
+        x_aug = x.copy()
+        for j in range(2,np.shape(x_aug)[0]-2):
+            if (np.mod(j,2)==0):
+                x_aug[j] = (x[j-1]*x[j-2]).copy()
+        out = np.squeeze(np.asarray(np.dot(Wout,x_aug)))
+        output[:,i] = out
+        x1 = np.tanh(np.dot(A,x) + np.dot(Win,out))
+        x = np.squeeze(np.asarray(x1))
+    return output, x
+
+data = np.load('./data/lorenz_63.npy')
+data = np.transpose(data)
+
+x,Wout,A,Win = train_reservoir(res_params,data[:,shift_k:shift_k+res_params['train_length']])
+
+output, _ = predict(A, Win,res_params,x,Wout)
+output_data = np.transpose(output)
+
---
+++ P_RC.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+import numpy as np
+import scipy.sparse as sparse
+from scipy.sparse import linalg
+import matplotlib.pyplot as plt
+
+model_params = {'tau': 0.025,
+                'nstep': 1000,
+                'N': 3,
+                'd': 3}
+
+res_params = {'radius':0.4,
+             'degree': 100,
+             'sigma': 1.0,
+             'Dr': 1000,
+             'train_length': 10000,
+             'predict_length': 5000,
+            'num_inputs': model_params['N'],
+             'beta': 0.00001
+              }
+# Randomly generated reservoir networks
+def generate_reservoir(size,radius,degree):
+    sparsity = degree/float(size);
+    A = sparse.rand(size,size,density=sparsity).todense()
+    vals = np.linalg.eigvals(A)
+    e = np.max(np.abs(vals))
+    A = (A/e) * radius
+    return A
+
+# The initial reservoir state space is activated using a nonlinear activation function. The procedure reads the training data to activate.
+def reservoir_layer(A, Win, input, res_params):
+    # Assigning values to the initial state space of the reservoir
+    states = np.zeros((res_params['train_length'],res_params['Dr']))
+    states[0] = np.zeros(res_params['Dr'])
+    # Cyclic update of reservoir state space by training time
+    for i in range(1, res_params['train_length']):
+        states[i] = np.tanh(A.dot(states[i-1]) + Win.dot(input[i-1]) )
+    # Retain the last moment state vector of the training time for subsequent predictions
+    states_nearfuture = np.tanh( A.dot(states[res_params['train_length']-1]) + Win.dot(input[res_params['train_length']-1]) )
+    return states,states_nearfuture
+
+# Training reservoir phase
+def train_reservoir(res_params, data):
+    A = generate_reservoir(res_params['Dr'], res_params['radius'], res_params['degree'])
+    q = int(res_params['Dr']/res_params['num_inputs'])
+    Win = np.zeros((res_params['Dr'],res_params['num_inputs']))
+    for i in range(res_params['num_inputs']):
+        np.random.seed(seed=i)
+        Win[i*q: (i+1)*q,i] = res_params['sigma'] * (-1 + 2 * np.random.rand(1,q)[0])
+    states,states_nearfuture = reservoir_layer(A, Win, data, res_params)
+    Wout = train(res_params, states, data)
+    return states_nearfuture, Wout, A, Win
+
+# Solve the weight matrix
+def train(res_params,states,data):
+    beta = res_params['beta']
+    RC_states = np.hstack( (states, np.power(states, 2), np.power(states, 3) ,np.power(states, 4),np.power(states, 5)) )
+    Wout =(data[0:res_params['train_length']].T).dot(RC_states).dot (np.linalg.inv(RC_states.T.dot(RC_states) + beta * np.eye(5 * res_params['Dr'])))
+    return Wout
+
+# Forecasting phase
+def predict(A, Win, res_params, states_nearfuture, Wout):
+    output = np.zeros((res_params['predict_length'], res_params['Dr']))
+    output_states = np.zeros((res_params['predict_length'], 5*res_params['Dr']))
+    output[0] = states_nearfuture
+    output_states[0] = np.hstack( (output[0], np.power(output[0], 2),np.power(output[0], 3),np.power(output[0], 4),np.power(output[0], 5)) )
+    # Autonomous forecasting phase
+    for i in range(1,res_params['predict_length']):
+        output[i] = np.tanh(A.dot(output[i-1]) + Win.dot( Wout.dot(output_states[i-1].T) ) )
+        output_states[i] = np.hstack( (output[i], np.power(output[i], 2),np.power(output[i], 3),np.power(output[i], 4),np.power(output[i], 5)) )
+    predict = Wout.dot(output_states.T)
+    return predict
+
+data = np.load('./data/lorenz_63.npy')
+
+states_nearfuture,Wout,A,Win = train_reservoir(res_params,data[:res_params['train_length'],:])
+
+output = predict(A, Win,res_params,states_nearfuture,Wout)
